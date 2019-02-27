b3f3625c027db77e2f85c762a2a67deb6d3d11d4
Roxolana <roxolanapavlyk@gmail.com>
2019-02-27 20:52:46 +0100

    take ApiKey from server env

--- portoapp/server/index.js
+++ portoapp/server/index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const cors = require('cors')
-
 const sgMail = require('@sendgrid/mail')
 const app = express()
 
-sgMail.setApiKey('')
+sgMail.setApiKey(process.env.SENDGRID_KEY)
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Welcome to my server')
 })
+
 app.get('/send-email', (req, res) => {
     const { recipient, sender, subject, text } = req.query;
     const msg = {
@@ -18,8 +18,9 @@ app.get('/send-email', (req, res) => {
         subject: subject,
         text: text
     }
+
     sgMail.send(msg)
         .then((msg) => console.log(text));
-
 })
+
 app.listen(4000, () => console.log("Running on Port 4000")); 
