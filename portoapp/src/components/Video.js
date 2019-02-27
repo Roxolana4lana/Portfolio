b04fe4ba04de8ad4f2ecd0013c9882b05b3c37fc
Roxolana <roxolanapavlyk@gmail.com>
2019-02-27 16:05:53 +0100

    background video component

---
+++ portoapp/src/components/Video.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import video from '../images/video.mp4'
+
+export default class Video extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <video className='videoTag' autoPlay loop muted>
+                    <source src={video} type='video/mp4' />
+                </video>
+            </React.Fragment>
+        )
+    }
+}
