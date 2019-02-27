76930c5f100be0ef09eab850b3c707d433757be2
Roxolana <roxolanapavlyk@gmail.com>
2019-02-27 15:55:31 +0100

    add component for link to github

---
+++ portoapp/src/components/ButtonLink.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class ButtonLink extends Component {
+    render() {
+        return (
+            <div>
+                <button className='hoveredLink' style={{ color: 'white' }}>
+                    <a style={{ textDecoration: 'none', color: 'white' }} href='https://github.com/Roxolana4lana?tab=repositories'>
+                        See the code
+        </a>
+                </button>
+            </div>
+        )
+    }
+}
