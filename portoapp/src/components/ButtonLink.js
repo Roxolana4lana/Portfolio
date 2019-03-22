d7c82471846c999d945972ec0df4cf78440629f2
Roxolana <roxolanapavlyk@gmail.com>
2019-03-22 11:31:01 +0100

    added new props

--- portoapp/src/components/ButtonLink.js
+++ portoapp/src/components/ButtonLink.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react'
 
 export default class ButtonLink extends Component {
     render() {
+        
         return (
             <div>
                 <button className='hoveredLink' style={{ color: 'white' }}>
-                    <a style={{ textDecoration: 'none', color: 'white' }} href='https://github.com/Roxolana4lana?tab=repositories'>
+                    <a style={{ textDecoration: 'none', color: 'white' }} href={this.props.href}>
                         See the code
                     </a>
                 </button>
