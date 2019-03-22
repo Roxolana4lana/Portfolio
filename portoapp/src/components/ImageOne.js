d412712ce6eb532337770d7ddb05bccd9e774d65
Roxolana <roxolanapavlyk@gmail.com>
2019-03-22 11:31:33 +0100

    added props

--- portoapp/src/components/ImageOne.js
+++ portoapp/src/components/ImageOne.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import im1 from '../images/image1.png'
 import im2 from '../images/image2.png'
 import im3 from '../images/image3.png'
+import im5 from '../images/im5.png'
 import im4 from '../images/image4.png'
 import Image from './Image'
 
@@ -14,36 +15,47 @@ export default class ImageOne extends Component {
                 {
                     'id': 1,
                     'name': im1,
-                    'project': 'Recipe App'
+                    'project': 'Recipe App',
+                    'href': 'https://github.com/Roxolana4lana/recpies'
                 },
                 {
                     'id': 2,
                     'name': im2,
-                    'project': 'Weather App'
+                    'project': 'Weather App',
+                    'href':'https://github.com/Roxolana4lana/weatherApp'
                 },
                 {
                     'id': 3,
                     'name': im3,
-                    'project': 'Expenses'
+                    'project': 'Expenses',
+                    'href': 'https://github.com/Roxolana4lana/reduxexpenses'
+                },
+                {
+                    'id': 5,
+                    'name': im5,
+                    'project': 'Image Gallery',
+                    'href': 'https://github.com/Roxolana4lana/imageGallery/tree/master/imageapp'
                 },
                 {
                     'id': 4,
                     'name': im4,
-                    'project': 'Portfolio'
+                    'project': 'Portfolio',
+                    'href': 'https://github.com/Roxolana4lana/Portfolio/tree/master/portoapp'
                 }
             ],
 
             image: {
                 'id': 1,
                 'name': im1,
-                'project': 'Wheather App'
+                'project': 'Wheather App',
+                'href': 'https://github.com/Roxolana4lana/weatherApp'
             },
             index: 0
         }
     }
 
     render() {
-        let myImage = this.state.images.map(i => (<div className="MyImage" key={i.id}> <Image name={i.name} title={i.project} /></div >))
+        let myImage = this.state.images.map(i => (<div className="MyImage" key={i.id}> <Image href={i.href}name={i.name} title={i.project} /></div >))
 
         return (
             <div className='wrapper'>
