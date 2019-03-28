2064e2d764be6c2d873955deda20c6ad130cc44e
Roxolana <roxolanapavlyk@gmail.com>
2019-03-28 13:16:05 +0100

    add footer

--- portoapp/src/components/Form.js
+++ portoapp/src/components/Form.js
@@ -1,106 +1,108 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import Footer from './Footer';
 
-
-export default class Form extends Component {
+export default class ContactForm extends Component {
     constructor() {
         super()
         this.state = {
-            mail: {
-                recipient: 'roxolanapavlyk@gmail.com',
-                sender: '',
-                subject: '',
-                text: '',
-            },
+            sender: '',
+            subject: '',
+            text: '',
             senderError: '',
-            subjectError:'',
-            textError:'' 
+            subjectError: '',
+            textError: '' ,
+            sent:''
+            
         }
     }
-
     handleMailChange = e => {
         this.setState({
-            mail: {
-                ...this.state.mail,
                 [e.target.name]: e.target.value
-            }
         })
-
     }
-    Validate = () =>{
-        let senderError=""
-        let subjectError=''
-        let textError=''
+    Validate = () => {
+        let senderError = ""
+        let subjectError = ''
+        let textError = ''
         let valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if (!valid.test(this.state.mail.sender)) {
-          senderError= 'type a valid email'
+        if (!valid.test(this.state.sender)) {
+            senderError = 'type a valid email'
         }
-          if (!this.state.mail.subject || this.state.mail.subject.length < 5){
-           
-                  subjectError= 'minimum 5 characters'
-          
-          }
-          if (!this.state.mail.text || this.state.mail.text.length < 5){
-   
-              textError = 'minimum 5 characters'
-     
-          }
+        if (!this.state.subject || this.state.subject.length < 5) {
 
-         if(senderError || textError || subjectError){  
-         console.log(this.state.senderError) 
+            subjectError = 'minimum 5 characters'
+
+        }
+        if (!this.state.text || this.state.text.length < 5) {
+
+            textError = 'minimum 5 characters'
+
+        }
+
+        if (senderError || textError || subjectError) {
+            console.log(this.state.senderError)
             this.setState({
                 senderError,
                 subjectError,
                 textError
             })
             return false
-          }
-          else{
-              return true 
-          }
-           
+        }
+        else {
+            return true
         }
 
-
+    }
     handleMailSubmit = e => {
-       e.preventDefault()
-     const valid =  this.Validate()
-        console.log(this.state.senderError) 
-        if(valid){
-            const { mail } = this.state
-            fetch(`http://127.0.0.1:4000/send-email?
-                recipient=${mail.recipient}
-                &sender=${mail.sender}
-                &subject=${mail.subject}
-                &text=${mail.text}`)
-                .catch(err => console.log(err))
-                .then(console.log('it is ok'))
-        }else
-        console.log('not valid')  
+        e.preventDefault()
+        const valid = this.Validate()
+        console.log(this.state.senderError)
+        if (valid) {
+       const mail ={
+           sender: this.state.sender,
+           subject:this.state.subject,
+           text:this.state.text
+       }
+        axios.post(`http://127.0.0.1:8000/send_email/`,mail)
+       .then(res=>console.log(res.data))
+            .catch(err => console.log(err))
+            .then(this.setState({
+                sender: '',
+                subject: '',
+                text: '',
+                senderError: '',
+                subjectError: '',
+                textError: '' ,
+                sent:'message sent!'
+            }))
+    }else
+    console.log('not valid')
     }
 
+
     render() {
-     
         return (
             <div className='mainForm'>
                 <h1>Contact me </h1>
                 <div className='myForm'>
                     <form onSubmit={this.handleMailSubmit}>
                         <div className="formPart">
-                            <label >Email</label>
+                            <label>Email</label>
                             <input type='text'
-                                value={this.state.mail.sender}
+                                value={this.state.sender}
                                 onChange={this.handleMailChange}
-                                name='sender'    
-                            /> 
+                                name='sender'
+                            />
                         </div>
                         <div className='errorMessage'><div></div>{this.state.senderError}</div>
                         <br />
                         <div className="formPart">
                             <label>Subject</label>
                             <input type='text'
-                                value={this.state.mail.subject}
+                                value={this.state.subject}
                                 onChange={this.handleMailChange}
-                                name='subject'  
+                                name='subject'
                             />
                         </div>
                         <div className='errorMessage'> <div></div>{this.state.subjectError}</div>
@@ -108,18 +110,21 @@ export default class Form extends Component {
                         <div className="formPart">
                             <label>Text</label>
                             <textarea
-                                value={this.state.mail.text}
+                                value={this.state.text}
                                 onChange={this.handleMailChange}
-                                name='text'  
+                                name='text'
                             />
                         </div>
                         <div className='errorMessage'><div></div>{this.state.textError}</div> 
                         <br />
-                        <button className='formButton' >Submit</button>
+                        <button className='formButton'>Submit</button>
+                        <div className='sendMessage'><div></div>{this.state.sent}</div> 
                     </form>
-                </div>
-            </div>
 
+                </div>
+                <div className='myContacts'></div>
+                <Footer/>
+            </div>
         )
     }
 }
