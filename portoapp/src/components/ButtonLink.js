b06098f3e1fbf66a2057fd8bcecc75680fbdd287
Roxolana <roxolanapavlyk@gmail.com>
2019-04-01 16:09:01 +0200

    clean up component

--- portoapp/src/components/ButtonLink.js
+++ portoapp/src/components/ButtonLink.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class ButtonLink extends Component {
-    render() {  
-        return (
-               <button className='hoveredLink' style={{ color: 'white',display:'inline' }}>
-                    <a style={{ textDecoration: 'none', color: 'white' }} href={this.props.href}>
-                        See the code
-                    </a>
-                </button>
-   
-        )
-    }
+  render() {
+    return (
+      <button
+        className="hoveredLink"
+        style={{ color: "white", display: "inline" }}
+      >
+        <a
+          style={{ textDecoration: "none", color: "white" }}
+          href={this.props.href}
+        >
+          See the code
+        </a>
+      </button>
+    );
+  }
 }
